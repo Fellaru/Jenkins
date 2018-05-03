95d22b1ff5366843fafe9d593df9718aa587b391
Ella Fischenko <efischenko@cinimex.ru>
2018-05-03 11:00:41 +0300

    add space

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by efischenko on 03.05.2018.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        for(int i=1; i<10; i++) {
+            System.out.println("Hello, Jenkins World! It is " + i + " time.");
+        }
+    }
+}

388b8f4c4d023c87665066d8687c8aaef4e791d4
Ella Fischenko <efischenko@cinimex.ru>
2018-05-03 16:44:17 +0300

    Ц!

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,22 @@
-node {
-sh "echo hello,World!"
+#!groovy
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
 }
